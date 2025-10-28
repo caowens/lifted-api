e1808449dee894cea92a264cafcff414ad02c773
caowens <caowens555@yahoo.com>
2025-10-27 22:48:45 -0700

    Setup basic GET route with express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Running on port 3000");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Lifted API!');
+});
+
+
+app.listen(3000, () => {
+    console.log('Lifted API is running on http://localhost:3000');
+});
+
+export default app;
