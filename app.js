568b996c08fbb61b9390371fdd729c3164e800da
caowens <caowens555@yahoo.com>
2025-10-28 23:35:49 -0700

    Add global error handling middleware

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import quoteRouter from './routes/quotes.routes.js';
 
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
+app.use(express.json()); // allows app to handle json data sent in API calls
+app.use(express.urlencoded({ extended: false })); // helps process html form data in a simple format
+app.use(cookieParser()); // to parse cookies from requests
+
+// Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/quotes', quoteRouter);
+
+app.use(errorMiddleware); // Error handling middleware
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Lifted API!');
